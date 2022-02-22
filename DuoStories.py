c69b888c9e291f38da270a901a0e02260ecb8911
Paiva <danpaiva@live.com>
2022-02-22 16:51:38 -0300

    Login Okay

--- DuoStories.py
+++ DuoStories.py
@@ -1,5 +1,18 @@
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Credentials
+user = 'danhpaiva'
+password = 'zzz'
 
 driver = webdriver.Chrome(executable_path='.\chromedriver.exe')
-driver.get("https://www.duolingo.com/")
+driver.get('https://www.duolingo.com/')
+
+buttonFirstPage = driver.find_element_by_xpath(
+    '//*[@id="root"]/div/div/span[1]/div/div[1]/div[2]/div[2]/button/span')
+buttonFirstPage.click()
+
+Login = driver.find_element_by_xpath(
+    '//*[@id="overlays"]/div[5]/div/div/form/div[1]/div[1]/div[1]/label/div/input')
+Login.send_keys(user + Keys.TAB + password + Keys.ENTER)
